24c7311fb785e9ac955807a3bdb7adf1f0c9b97f
Humberto STEIN SHIROMOTO <h.stein.shiromoto@gmail.com>
2022-04-25 18:07:05 +1000

    minor changes

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import pathlib
 from setuptools import setup
 
 # The directory containing this file
-HERE = pathlib.Path(__file__).resolve().parent
+HERE = pathlib.Path(__file__).parent
 
 # The text of the README file
 README = (HERE / "README.md").read_text()
@@ -12,12 +12,12 @@ setup(
     name="tufte",
     version="0.1.0",
     description="A Tufte-inspired style for plotting data.",
-    long_description_content_type="text/markdown",
     long_description=README,
+    long_description_content_type="text/markdown",
     url="https://github.com/hsteinshiromoto/tufte",
     author="Humberto STEIN SHIROMOTO",
     author_email="h.stein.shiromoto@gmail.com",
-    # license="GPL",
+    license="GPL",
     classifiers=[
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
