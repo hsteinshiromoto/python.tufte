61593b94f8e3c0981ce1e7277e620f3066943d73
Humberto STEIN SHIROMOTO <h.stein.shiromoto@gmail.com>
2022-04-27 14:00:38 +1000

    sort imports

--- tufte/__init__.py
+++ tufte/__init__.py
@@ -1,5 +1,5 @@
 __version__ = "0.2.1"
 
-from tufte.line import main as lineplot
 from tufte.box import main as boxplot
+from tufte.line import main as lineplot
 from tufte.scatter import main as scatterplot
