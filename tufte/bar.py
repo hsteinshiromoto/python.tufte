91672d785ab5d90da38321ea820f38112595321e
Humberto STEIN SHIROMOTO <h.stein.shiromoto@gmail.com>
2022-04-27 15:20:17 +1000

    remove ticks settings

--- tufte/bar.py
+++ tufte/bar.py
@@ -36,10 +36,6 @@ class Bar(Plot):
 
         self.ax.bar(x, y, align=align, color=color, edgecolor=edgecolor, width=width)
 
-        axis_values_dict = self.fit_axis_values(x, y)
-        self.set_ticks(axis_values_dict["x"]["range"], align, width, gridcolor)
-        self.set_axis(xlim=(0, 0), xbounds=(0, 0), ylim=(0, 0), ybounds=(0, 0))
-
         return self.ax
 
     def set_Bar_spines(self):
@@ -50,58 +46,6 @@ class Bar(Plot):
     def set_plot_title(self, title: str = None):
         title = title or f"{Bar.__name__} plot of {self.xlabel} and {self.ylabel}"
         super().set_plot_title(title)
-
-    def set_ticks(
-        self,
-        xrange: Iterable[int | float],
-        align: str,
-        width: float,
-        gridcolor: str,
-    ):
-        if align.lower() == "center":
-            lower_buffer = 0.5
-            upper_buffer = 0.5
-
-        elif align.lower() == "edge":
-            lower_buffer = 0.25
-            upper_buffer = width + 0.25
-
-        else:
-            msg = f"Expected align to be either center of edge. Got {align}."
-            raise ValueError(msg)
-
-        xmin = min(xrange)
-        xmax = max(xrange)
-
-        xlist = [
-            xl for xl in self.ax.xaxis.get_majorticklocs() if xl >= xmin and xl <= xmax
-        ]
-        xlist = [xmin - lower_buffer] + xlist[1:-1] + [xmax + upper_buffer]
-        yticklocs = self.ax.yaxis.get_majorticklocs()
-        for y in yticklocs:
-            self.ax.plot([xlist[0], xlist[-1]], [y, y], color=gridcolor, linewidth=1.25)
-
-    def fit_axis_values(
-        self,
-        x: Iterable[int | float | str],
-        y: Iterable[int | float | str],
-    ):
-        """Calculates plot limits and axes bounds.
-
-        Args:
-            x (Iterable[int  |  float | str]): x axes iterable.
-            y (Iterable[int  |  float | str]): y axes iterable.
-
-        Returns:
-            _type_: _description_
-        """
-        xvalues = np.unique(x) if isinstance(next(x), str) else x
-        yvalues = np.unique(y) if isinstance(next(y), str) else y
-
-        return {
-            "x": {"range": range(len(xvalues))},
-            "y": {"range": range(len(yvalues))},
-        }
 
     def auto_rotate_xticklabel(self):
         figw = self.fig.get_figwidth()
