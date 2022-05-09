fde1917b34ff2d7a16cbf696ca92807316123d49
Humberto STEIN SHIROMOTO <h.stein.shiromoto@gmail.com>
2022-05-10 09:34:48 +1000

    improve make reference

--- tests/test_bar.py
+++ tests/test_bar.py
@@ -1,65 +1,48 @@
 import sys
 from pathlib import Path
+from collections.abc import Iterable
 
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.testing.decorators import image_comparison, check_figures_equal
 from PIL import Image
+import pytest
+import tempfile
+from matplotlib.axes import Axes
 
 PROJECT_ROOT = Path.cwd().resolve().parent
 sys.path.append(str(PROJECT_ROOT))
 
 from tufte.bar import main as barplot
 
-REFERENCE_FILENAME = "reference_bar.png"
+REFERENCE_IMAGE = "reference_bar.png"
 
 
-def make_reference_figure():
+def make_figure(x: Iterable, y: Iterable) -> Axes:
+    return barplot(x, y)
+
+
+@pytest.fixture(scope="module")
+def make_reference_figure(filename: str = REFERENCE_IMAGE) -> Path:
     x = ["A", "B", "C", "D", "E", "F"]
-    y = np.array([41, 23, 48, 84, 32, 38])
+    y = np.array([41, 23, 48, 84, 32, 38]).flatten()
 
-    ax = barplot(x, y.flatten())
+    ax = make_figure(x, y)
 
-    plt.savefig(REFERENCE_FILENAME)
+    path = Path(tempfile.mkdtemp())
 
-    return ax
+    plt.savefig(str(path / filename))
 
-
-def get_figure(filename: Path):
-    with open(str(filename), "r") as f:
-        figure = f.read()
-
-    return figure
+    return path
 
 
-@check_figures_equal()
-def test_plot(fig_test, fig_ref):
-    fig_test.subplots().plot([1, 3, 5])
-    fig_ref.subplots().plot([0, 1, 2], [1, 3, 5])
+@pytest.fixture
+def call_reference_figure(request):
+    make_reference_figure(request.param)
 
 
-@image_comparison(
-    baseline_images=["make_reference_figure"], remove_text=True, extensions=["png"]
-)
-def test_line_dashes():
-    ax = make_reference_figure()
-
-
-def test_figure():
-    make_reference_figure()
-    x = ["A", "B", "C", "D", "E", "F"]
-    y = np.array([41, 23, 48, 84, 32, 38])
-
-    ax = barplot(x, y.flatten())
-    plt.savefig("test_bar.png")
-
-    reference_figure = get_figure(REFERENCE_FILENAME)
-    test_figure = get_figure("test_bar.png")
-
-    assert test_figure == reference_figure
-
-
-def test_images_equal(image_1: str = REFERENCE_FILENAME, image_2: str = "test_bar.png"):
+@pytest.mark.parametrize("call_reference_figure", [REFERENCE_IMAGE, "test_bar.png"])
+def test_images_equal(call_reference_figure):
     """_summary_
 
     Args:
@@ -70,20 +53,18 @@ def test_images_equal(image_1: str = REFERENCE_FILENAME, image_2: str = "test_ba
         [1] https://www.redshiftzero.com/pytest-image/
 
     """
-    img1 = Image.open(image_1)
-    img2 = Image.open(image_2)
+    reference_image = Image.open(REFERENCE_IMAGE)
+    img2 = Image.open("test_bar.png")
 
     # Convert to same mode and size for comparison
-    img2 = img2.convert(img1.mode)
-    img2 = img2.resize(img1.size)
+    img2 = img2.convert(reference_image.mode)
+    img2 = img2.resize(reference_image.size)
 
     sum_sq_diff = np.sum(
-        (np.asarray(img1).astype("float") - np.asarray(img2).astype("float")) ** 2
+        (np.asarray(reference_image).astype("float") - np.asarray(img2).astype("float"))
+        ** 2
     )
 
-    if sum_sq_diff == 0:
-        # Images are exactly the same
-        assert True
-    else:
+    if sum_sq_diff != 0:
         normalized_sum_sq_diff = sum_sq_diff / np.sqrt(sum_sq_diff)
         assert normalized_sum_sq_diff < 0.001
